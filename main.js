f8c5abfd12e7675330795c53b309adb53d06ac8c
srdjanmaljukan <srdjanmaljukan96@gmail.com>
2022-06-22 00:43:05 +0200

    write fieldArray

--- main.js
+++ main.js
@@ -15,11 +15,27 @@ class Field {
     }
   
     generateField(width, height) {
+        let fieldArray = [];
       for (i = 0; i < height.length; i++) {
         for (j = 0; j < width.length; j++) {
-          
+          fieldArray.push(Math.floor(Math.random() * 9));
         }
       }
     }
   }
+
+function generateField(width, height) {
+    let fieldArray = [];
+    for (let i = 0; i < height; i++) {
+        let rowArray = [];
+        for (let j = 0; j < width; j++) {
+            let random = Math.floor(Math.random() * 9);
+            arr.push(random);
+        }
+        field.push(arr);
+    }
+    return field;
+}
+
+console.log(generateField(10, 10));
   
