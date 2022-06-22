cadcd46283238cf620238251eec84925f5d033f6
srdjanmaljukan <srdjanmaljukan96@gmail.com>
2022-06-22 02:10:14 +0200

    add print and generateField methods

--- main.js
+++ main.js
@@ -6,36 +6,42 @@ const fieldCharacter = '░';
 const pathCharacter = '*';
 
 class Field {
-    constructor(field) {
+    constructor(field = [[]]) {
       this.field = field;
+      this.locationX = 0;
+      this.locationY = 0;
+      this.field[0][0] = pathCharacter;
     }
   
     print() {
-      console.log(this.field.join(''));
+      const displayField = this.field.map(row => {
+        return row.join('');
+      }).join('\n');
+      console.log(displayField);
     }
   
-    generateField(width, height) {
-        let fieldArray = [];
-      for (i = 0; i < height.length; i++) {
-        for (j = 0; j < width.length; j++) {
-          fieldArray.push(Math.floor(Math.random() * 9));
+    static generateField(width, height, percentage = 0.2) {
+        const field = new Array(height).fill(0).map(el => new Array(width));
+        for (let y = 0; y < height; y++) {
+          for (let x = 0; x < width; x++) {
+            const prob = Math.random();
+            field[y][x] = prob > percentage ? fieldCharacter : hole;
+          }
         }
-      }
+
+        const hatLocation = {
+          x: Math.floor(Math.random() * width),
+          y: Math.floor(Math.random() * height)
+        }
+        while (hatLocation.x === 0 && hatLocation.y === 0) {
+          hatLocation.x = Math.floor(Math.random() * width);
+          hatLocation.y = Math.floor(Math.random() * height);
+        }
+        field[hatLocation.y][hatLocation.x] = hat;
+        return field;
     }
   }
 
-function generateField(width, height) {
-    let fieldArray = [];
-    for (let i = 0; i < height; i++) {
-        let rowArray = [];
-        for (let j = 0; j < width; j++) {
-            let random = Math.floor(Math.random() * 9);
-            arr.push(random);
-        }
-        field.push(arr);
-    }
-    return field;
-}
-
-console.log(generateField(10, 10));
+const field = new Field(Field.generateField(20, 10, 0.2));
+console.log(field.print());
   
