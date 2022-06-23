a444af68053b4b30a2ce1adc1e7f139b72045fd9
srdjanmaljukan <srdjanmaljukan96@gmail.com>
2022-06-23 22:39:01 +0200

    make player character spawn at random location

--- main.js
+++ main.js
@@ -8,9 +8,9 @@ const pathCharacter = '*';
 class Field {
     constructor(field = [[]]) {
       this.field = field;
-      this.locationX = 0;
-      this.locationY = 0;
-      this.field[0][0] = pathCharacter;
+      this.locationX = Math.floor(Math.random() * this.field[0].length);
+      this.locationY = Math.floor(Math.random() * this.field.length);
+      this.field[this.locationY][this.locationX] = pathCharacter;
     }
   
     print() {
